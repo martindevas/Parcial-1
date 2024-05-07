b4e4a01e77970ef8d90dd04c28eaafa57b9d65c4
martindevas <martindevas45@gmail.com>
2024-05-07 19:28:01 -0300

    Agrego paginas vacias y mainpage

---
+++ app/src/main/java/com/example/parcial1/ContraseñaCorrecta.kt
@@ -0,0 +1,23 @@
+package com.example.parcial1
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.parcial1.ui.theme.Parcial1Theme
+
+@Composable
+fun ContraseñaCorrecta(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hola",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContraseñaCorrectaPreview() {
+    Parcial1Theme {
+        ContraseñaCorrecta()
+    }
+}
--- app/src/main/java/com/example/parcial1/MainActivity.kt
+++ app/src/main/java/com/example/parcial1/MainActivity.kt
@@ -22,25 +22,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainPage()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Parcial1Theme {
-        Greeting("Android")
-    }
-}
