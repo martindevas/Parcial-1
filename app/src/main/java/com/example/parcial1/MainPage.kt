f2b485515195389954779f8d4057ff2b24d26d56
martindevas <martindevas45@gmail.com>
2024-05-07 19:36:58 -0300

    Agrego Usuraio y contraseña

--- app/src/main/java/com/example/parcial1/MainPage.kt
+++ app/src/main/java/com/example/parcial1/MainPage.kt
@@ -1,17 +1,54 @@
 package com.example.parcial1
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.parcial1.ui.theme.Parcial1Theme
 
 @Composable
 fun MainPage(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hola",
-        modifier = modifier
-    )
+    Column(modifier.padding(horizontal = 30.dp)) {
+        Text(
+            text = "¡Hola por favor, ingresa clave y contraseña!",
+            modifier = modifier.padding(vertical = 10.dp)
+        )
+        Row(modifier.padding()) {
+            Text(
+                text = "Ingresa a tu cuenta",
+                modifier = modifier
+            )
+        }
+        TextField(
+            value = "Email", onValueChange = {}, modifier = modifier
+                .padding(top = 10.dp)
+                .fillMaxWidth()
+        )
+        TextField(
+            value = "Contraseña", onValueChange = {}, modifier = modifier
+                .padding(top = 10.dp)
+                .fillMaxWidth()
+        )
+        Button(
+            onClick = { }, modifier = modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(10.dp)
+        ) {
+            Text(
+                text = "Ingresar ",
+                modifier = modifier.padding(horizontal = 20.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
