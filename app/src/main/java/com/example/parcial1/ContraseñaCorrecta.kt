cb26c0a14fdf02bbdd0753865ffbff9ee6fb6815
martindevas <martindevas45@gmail.com>
2024-05-07 20:54:50 -0300

    Dejo listas las vistas, falta navegacion

--- app/src/main/java/com/example/parcial1/ContraseñaCorrecta.kt
+++ app/src/main/java/com/example/parcial1/ContraseñaCorrecta.kt
@@ -1,32 +1,34 @@
 package com.example.parcial1
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.parcial1.ui.theme.Parcial1Theme
+
 
 @Composable
 fun ContraseñaCorrecta(
     modifier: Modifier = Modifier,
     navController: NavController
 ) {
-    Column {
-        Text(text = "ingresaste!")
-        Button(
-            onClick = {
-                navController.navigate("MainPage")
-            }
-        ) {
-            Text(text = "volver a MainPage")
-        }
-    }
+    Column(
+        modifier = modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "¡Bienvenido usuario: Pedro Pe!")
+    },
 }
-
 @Preview(showBackground = true)
 @Composable
 fun ContraseñaCorrectaPreview() {
