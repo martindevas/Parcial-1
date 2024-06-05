6630e5a8356e65606c208acaab4e7601d5db4215
martindevas <martindevas45@gmail.com>
2024-06-05 15:49:42 -0300

    Amplio pantallas y hago funcionar la navegacion

--- app/src/main/java/com/example/parcial1/ContraseñaCorrecta.kt
+++ app/src/main/java/com/example/parcial1/ContraseñaCorrecta.kt
@@ -1,39 +1,56 @@
 package com.example.parcial1
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.unit.sp
 import com.example.parcial1.ui.theme.Parcial1Theme
 
 
 @Composable
 fun ContraseñaCorrecta(
-    modifier: Modifier = Modifier,
-    navController: NavController
+    usuario: String
 ) {
     Column(
-        modifier = modifier.padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize(),
     ) {
-        Text(
-            text = "¡Bienvenido usuario: Pedro Pe!")
-    },
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Accediste con exito",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(20.dp)
+            )
+            Text(
+                text = "¡Bienvenido!",
+                fontSize = 40.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(20.dp)
+            )
+            Text(
+                text = "Pedro Pe",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(20.dp)
+            )
+        }
+
+    }
 }
 @Preview(showBackground = true)
 @Composable
 fun ContraseñaCorrectaPreview() {
     Parcial1Theme {
-        val navController = rememberNavController()
-        ContraseñaCorrecta(navController = navController)
+        ContraseñaCorrecta("")
     }
 }
--- app/src/main/java/com/example/parcial1/MainPage.kt
+++ app/src/main/java/com/example/parcial1/MainPage.kt
@@ -1,16 +1,22 @@
 package com.example.parcial1
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -18,7 +24,56 @@ import com.example.parcial1.ui.theme.Parcial1Theme
 
 @Composable
 fun MainPage(modifier: Modifier = Modifier) {
-    Column(modifier.padding(horizontal = 30.dp)) {
+    val navHostController = rememberNavController()
+    Scaffold(
+        modifier = Modifier
+    ) {
+        MainNavHost(
+            modifier = Modifier.padding(it),
+            navHostController = navHostController,
+        )
+    }
+}
+
+@Composable
+fun MainNavHost(
+    modifier: Modifier = Modifier,
+    navHostController: NavHostController,
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navHostController,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            Ingreso(navHostController, modifier = modifier)
+        }
+        composable("home") {
+            ContraseñaCorrecta(
+                usuario = "Pedro Pe"
+            )
+        }
+    }
+}
+fun VerificacionUsuario(usuario: String, contraseña: String): Boolean {
+    val usuarioAValidar = "pedro@pe.com.ar"
+    val contraseñaAValidar = "abc123"
+    return usuario == usuarioAValidar && contraseña == contraseñaAValidar
+}
+
+@Composable
+fun Ingreso(
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    val usuarioIngresado = remember { mutableStateOf("") }
+    val contraseñaIngresada = remember { mutableStateOf("") }
+    val error = remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         Text(
             text = "Hola!",
             modifier = modifier.padding(vertical = 10.dp)
@@ -29,24 +84,38 @@ fun MainPage(modifier: Modifier = Modifier) {
                 modifier = modifier
             )
         }
-        TextField(
-            value = "Email", onValueChange = {}, modifier = modifier
-                .padding(top = 10.dp)
-                .fillMaxWidth()
+        OutlinedTextField(
+            value = usuarioIngresado.value,
+            onValueChange = { usuarioIngresado.value = it },
+            label = { Text("Email") },
+            modifier = Modifier.padding(bottom = 10.dp)
         )
-        TextField(
-            value = "Clave", onValueChange = {}, modifier = modifier
-                .padding(top = 10.dp)
-                .fillMaxWidth()
+        OutlinedTextField(
+            value = contraseñaIngresada.value,
+            onValueChange = { contraseñaIngresada.value = it },
+            label = { Text("Clave") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.padding(bottom = 8.dp)
         )
         Button(
-            onClick = { }, modifier = modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(10.dp)
+            onClick = {
+                val usuario = usuarioIngresado.value
+                val contraseña = contraseñaIngresada.value
+                if (VerificacionUsuario(usuario, contraseña)) {
+                    error.value = false
+                    navHostController.navigate("home")
+                } else {
+                    error.value = true
+                }
+            },
+            modifier = Modifier.padding(top = 16.dp)
         ) {
+            Text("Ingresar")
+        }
+        if (error.value) {
             Text(
-                text = "Ingresar ",
-                modifier = modifier.padding(horizontal = 20.dp)
+                text = "Usuario o contraseña incorrectos!",
+                modifier = Modifier.padding(top = 8.dp)
             )
         }
     }
